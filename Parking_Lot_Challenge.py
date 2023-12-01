0bb442d8e6da9dc7780b35dc053abd1b2ead8cd4
Sefinsha-b <Sefinsha4@gmail.com>
2023-12-01 18:21:58 +0530

    Body Class Created

---
+++ Parking_Lot_Challenge.py
@@ -0,0 +1,27 @@
+import random
+import json
+
+
+class ParkingLot:
+    # size based on squarerecbBox
+    def __init__(self, squarefootage, size=(8,12)):
+
+
+
+    def ParkCar(self,car,spot):
+
+
+    def map_t_json(self):
+
+        
+        
+
+class Car:
+
+    def __init__(self,license_plates) :
+
+        
+    def Park(self,ParkingLot):
+
+def main():
+    
